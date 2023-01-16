38b13dd0fc99af2138c3376beb43e0abc5aefcc9
Brian <bjm008@lvc.edu>
2023-01-16 09:48:24 -0500

    HW1 work (not actually)

--- CSC 242/HW/1.1-2.1/main.cpp
+++ CSC 242/HW/1.1-2.1/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
+    vector<int> test;
+    test.push_back(1);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
