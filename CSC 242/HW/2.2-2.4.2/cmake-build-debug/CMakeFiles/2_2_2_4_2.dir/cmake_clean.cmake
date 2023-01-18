ef1b9dbb85fb5533545ef89d8dc59ef8bbf53e7e
Brian Myers <brianmyers691@gmail.com>
2023-01-18 08:50:26 -0500

    Class 1-18

---
+++ CSC 242/HW/2.2-2.4.2/cmake-build-debug/CMakeFiles/2_2_2_4_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "2_2_2_4_2"
+  "2_2_2_4_2.pdb"
+  "CMakeFiles/2_2_2_4_2.dir/main.cpp.o"
+  "CMakeFiles/2_2_2_4_2.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_2_2_4_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
