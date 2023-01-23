a8c7c931938b482fb52cf435748c530504b4973f
Brian Myers <brianmyers691@gmail.com>
2023-01-23 08:06:33 -0500

    Class setup 1/23

--- CSC 242/In Class/1-20/cmake-build-debug/cmake_install.cmake
+++ CSC 242/In Class/1-20/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/LVC/bjm008/CLionProjects/LVC-CDS/CSC 242/HW/2.2-2.4.2
+# Install script for directory: /home/LVC/bjm008/CLionProjects/LVC-CDS/CSC 242/In Class/1-20
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/LVC/bjm008/CLionProjects/LVC-CDS/CSC 242/HW/2.2-2.4.2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/LVC/bjm008/CLionProjects/LVC-CDS/CSC 242/In Class/1-20/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
