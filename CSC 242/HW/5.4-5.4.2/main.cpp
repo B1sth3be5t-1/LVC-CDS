4b609e8012e57d91aa5428c413a393ea67fc7b10
Brian <bjm008@lvc.edu>
2023-02-27 13:13:13 -0500

    HW 3-1 NOT 2/29 bc im stupid

--- CSC 242/HW/5.4-5.4.2/main.cpp
+++ CSC 242/HW/5.4-5.4.2/main.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
+#include "QuadraticProbing.h"
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+// Simple main
+int main( )
+{
+    //pt 1:
+    /*
+     * Given input {4371, 1323, 6173, 4199, 4344, 9679, 1989} and a hash function
+        h(x) = x (mod () 10), show the resulting
+
+        b)
+
+        0) 9679
+        1) 4371
+        2) 1989
+        3) 1323
+        4) 6173
+        5) 4344
+        6)
+        7)
+        8)
+        9) 4199
+
+        c)
+
+        0) 9679
+        1) 4371
+        2)
+        3) 1323
+        4) 6173
+        5) 4344
+        6)
+        7)
+        8) 1989
+        9) 4199
+     */
+
+    HashTable<int> h1;
+    HashTable<int> h2;
+
+    const int NUMS = 4000;
+    const int GAP  =   37;
+    int i;
+
+    cout << "Checking... (no more output means success)" << endl;
+
+    for( i = GAP; i != 0; i = ( i + GAP ) % NUMS )
+        h1.insert( i );
+
+    h2 = h1;
+
+    for( i = 1; i < NUMS; i += 2 )
+        h2.remove( i );
+
+    for( i = 2; i < NUMS; i += 2 )
+        if( !h2.contains( i ) )
+            cout << "Contains fails " << i << endl;
+
+    for( i = 1; i < NUMS; i += 2 )
+    {
+        if( h2.contains( i ) )
+            cout << "OOPS!!! " <<  i << endl;
+    }
+
     return 0;
 }
