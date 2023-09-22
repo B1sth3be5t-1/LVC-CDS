402bc21b6aace8b0e4555b0a4117844d45edbfd6
Brian Myers <brianmyers691@gmail.com>
2023-09-22 09:18:13 -0400

    6Awkward, still too slow

--- CSC 285/6Awkward.dart
+++ CSC 285/6Awkward.dart
@@ -3,46 +3,34 @@ import 'dart:io';
 void main(List<String> args) {
   String inp = stdin.readLineSync()!;
   int numSeats = int.parse(inp);
-  Map<int, List<int>> map = {};
-  List<int> lis = [];
+  Map<int, int> map = {};
 
   inp = stdin.readLineSync()!;
   List<String> strs = inp.split(' ');
 
+  int minDis = -1;
   int index = 0;
 
   for (String s in strs) {
     int num = int.parse(s);
 
-    if (!map.containsKey(num))
-      map[num] = [index];
-    else
-      map[num]!.add(index);
+    if (map.containsKey(num) && (minDis == -1 || index - map[num]! < minDis)) {
+      minDis = index - map[num]!;
+    }
 
-    lis.add(num);
+    if (minDis == 1) {
+      print(1);
+      return;
+    }
+
+    map[num] = index;
+
     index++;
   }
 
-  int minDis = -1;
   if (map.length == numSeats) {
     print(numSeats);
     return;
-  }
-
-  for (MapEntry<int, List<int>> me in map.entries) {
-    if (me.value.length == 1) continue;
-    int minDisInner = -1;
-
-    for (int i = 0; i < me.value.length - 1; i++)
-      if (minDisInner == -1 || me.value[i + 1] - me.value[i] < minDisInner) {
-        minDisInner = me.value[i + 1] - me.value[i];
-        if (minDisInner == 1) {
-          print(1);
-          return;
-        }
-      }
-
-    if (minDis == -1 || minDisInner < minDis) minDis = minDisInner;
   }
 
   print(minDis);
